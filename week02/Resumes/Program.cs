0d8846b3e368748582983d5972432b7de9e1ad28
Ayah kodjo  Ayitey <67638556+AyahAyitey@users.noreply.github.com>
2026-09-04 16:42:32 +0000

    Update Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,17 +7,17 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._startYear = 2015;
+        job1._endYear = 2020;
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._startYear = 2021;
+        job2._endYear = 2025;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Ayitey Ayitey";
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
